cca31f215dc89607f7d0ab70da59a899eae90140
thuandinhdev <97577789+thuandinhdev@users.noreply.github.com>
2022-02-01 22:03:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -10,36 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    if (file_exists(storage_path('installed'))) {
-        if (!is_null(config('app.front_url'))) {
-            return redirect(config('app.front_url'));
-        }
-        return response()->json('Success');
-    } else {
-        Artisan::call('config:clear', []);
-        \View::addLocation(base_path().'/Modules/Installer/Views');
-        \View::addNamespace('theme', base_path().'/Modules/Installer/Views');
-        return View::make('theme::welcome');
-    }
-})->name('index');
-
-// Application cache cleared.
-Route::get('/cache-clear', function() {
-    Artisan::call('cache:clear');
-    return "Application cache cleared!";
-});
-
-// Configuration cached.
-Route::get('/config-cache', function() {
-    Artisan::call('config:cache');
-    return "Configuration cache cleared!<br>Configuration cached successfully!";
-});
-
-// Configuration cached.
-Route::get('/queue-work', function() {
-    $r = Artisan::call('queue:work');
-    pr($r);
-    return "Done!";
+    return view('welcome');
 });
