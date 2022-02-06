0b309c584b944f29f0c3dc9577658c2c5747acc9
thuandinhdev <thuandinhdev@gmail.com>
2022-02-06 10:56:43 +0700

    update

--- Modules/Menu/Repositories/MenuRepository.php
+++ Modules/Menu/Repositories/MenuRepository.php
@@ -171,7 +171,6 @@ class MenuRepository
 
         // --
         // Get departments/roles
-        var_dump(Auth::user());
         if (!empty(\Auth::user())) {
             $roles = \Auth::user()->roles()->get()->pluck('id');
             $departments = \Auth::user()->departments()->get()->pluck('id');
