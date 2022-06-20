b401f498d3ef6575062c529ea022b87f0710254b
thuandinhdev <97577789+thuandinhdev@users.noreply.github.com>
2022-06-20 09:44:14 +0700

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -42,8 +42,7 @@ return [
         ],
 
         'api' => [
-//             'driver' => 'jwt',
-            'driver' => 'session',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
